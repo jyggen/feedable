8af463bf719d3ef1279a6c7b6272418bc57156bb
Jonas Stendahl <jonas.stendahl@gmail.com>
2014-01-09 01:10:27 +0100

    Improved tests for RDF.

--- tests/Formatter/RDFTest.php
+++ tests/Formatter/RDFTest.php
@@ -1,8 +1,7 @@
 <?php
 namespace Feedable\Tests\Formatter;
 
-use DateTime;
-use Feedable\Feed;
+use DOMDocument;
 use Feedable\Formatter\RDF;
 
 class RDFTest extends \PHPUnit_Framework_TestCase
@@ -12,143 +11,104 @@ class RDFTest extends \PHPUnit_Framework_TestCase
         new RDF;
     }
 
-    public function testBootstrap()
+    public function rdfProvider()
     {
-        $feed      = new Feed(new RDF);
-        $generator = Feed::URI;
-        $date      = DateTime::createFromFormat('Y-m-d', date('Y-m-d'))->format(DateTime::ATOM);
-        $output    = <<<RSS
-<?xml version="1.0" encoding="UTF-8"?>
-<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:admin="http://webns.net/mvcb/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns="http://purl.org/rss/1.0/">
-  <channel rdf:about="http://example.com/">
-    <admin:generatorAgent rdf:resource="$generator"/>
-    <dc:date>$date</dc:date>
-    <sy:updatePeriod>hourly</sy:updatePeriod>
-    <sy:updateFrequency>1</sy:updateFrequency>
-    <sy:updateBase>2000-01-01T12:00+00:00</sy:updateBase>
-    <items/>
-  </channel>
-</rdf:RDF>
+        $document  = new DOMDocument;
+        $formatter = new RDF;
 
-RSS;
+        $document->formatOutput = true;
+        $formatter->bootstrap($document);
 
-        $feed->formatOutput(true);
-        $this->assertSame($output, $feed->render());
-    }
-
-    public function feedProvider()
-    {
-        return array(array(new Feed(new RDF)));
+        return array(array($formatter, $document));
     }
 
     /**
-     * @dataProvider feedProvider
+     * @dataProvider rdfProvider
      */
-    public function testSetBaseUrl($feed)
+    public function testBootstrap($rdf, $document)
     {
-        $generator = Feed::URI;
-        $date      = DateTime::createFromFormat('Y-m-d', date('Y-m-d'))->format(DateTime::ATOM);
-        $output    = <<<RSS
-<?xml version="1.0" encoding="UTF-8"?>
-<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:admin="http://webns.net/mvcb/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns="http://purl.org/rss/1.0/">
+        $output = <<<Feed
+<?xml version="1.0"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://purl.org/rss/1.0/">
+  <channel rdf:about="http://example.com/"/>
+</rdf:RDF>
+
+Feed;
+
+        $this->assertSame($output, $document->saveXML());
+    }
+
+    /**
+     * @dataProvider rdfProvider
+     */
+    public function testSetBaseUrl($rdf, $document)
+    {
+        $output = <<<Feed
+<?xml version="1.0"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://purl.org/rss/1.0/">
   <channel rdf:about="http://example.com/">
     <link>foobar</link>
-    <admin:generatorAgent rdf:resource="$generator"/>
-    <dc:date>$date</dc:date>
-    <sy:updatePeriod>hourly</sy:updatePeriod>
-    <sy:updateFrequency>1</sy:updateFrequency>
-    <sy:updateBase>2000-01-01T12:00+00:00</sy:updateBase>
-    <items/>
   </channel>
 </rdf:RDF>
 
-RSS;
+Feed;
 
-        $feed->formatOutput(true);
-        $feed->setBaseUrl('foobar');
-        $this->assertSame($output, $feed->render());
+        $rdf->setBaseUrl('foobar');
+        $this->assertSame($output, $document->saveXML());
     }
 
     /**
-     * @dataProvider feedProvider
+     * @dataProvider rdfProvider
      */
-    public function testSetDescription($feed)
+    public function testSetDescription($rdf, $document)
     {
-        $generator = Feed::URI;
-        $date      = DateTime::createFromFormat('Y-m-d', date('Y-m-d'))->format(DateTime::ATOM);
-        $output    = <<<RSS
-<?xml version="1.0" encoding="UTF-8"?>
-<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:admin="http://webns.net/mvcb/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns="http://purl.org/rss/1.0/">
+        $output = <<<Feed
+<?xml version="1.0"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://purl.org/rss/1.0/">
   <channel rdf:about="http://example.com/">
     <description>foobar</description>
-    <admin:generatorAgent rdf:resource="$generator"/>
-    <dc:date>$date</dc:date>
-    <sy:updatePeriod>hourly</sy:updatePeriod>
-    <sy:updateFrequency>1</sy:updateFrequency>
-    <sy:updateBase>2000-01-01T12:00+00:00</sy:updateBase>
-    <items/>
   </channel>
 </rdf:RDF>
 
-RSS;
+Feed;
 
-        $feed->formatOutput(true);
-        $feed->setDescription('foobar');
-        $this->assertSame($output, $feed->render());
+        $rdf->setDescription('foobar');
+        $this->assertSame($output, $document->saveXML());
     }
 
     /**
-     * @dataProvider feedProvider
+     * @dataProvider rdfProvider
      */
-    public function testSetTitle($feed)
+    public function testSetTitle($rdf, $document)
     {
-        $generator = Feed::URI;
-        $date      = DateTime::createFromFormat('Y-m-d', date('Y-m-d'))->format(DateTime::ATOM);
-        $output    = <<<RSS
-<?xml version="1.0" encoding="UTF-8"?>
-<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:admin="http://webns.net/mvcb/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns="http://purl.org/rss/1.0/">
+        $output = <<<Feed
+<?xml version="1.0"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://purl.org/rss/1.0/">
   <channel rdf:about="http://example.com/">
     <title>foobar</title>
-    <admin:generatorAgent rdf:resource="$generator"/>
-    <dc:date>$date</dc:date>
-    <sy:updatePeriod>hourly</sy:updatePeriod>
-    <sy:updateFrequency>1</sy:updateFrequency>
-    <sy:updateBase>2000-01-01T12:00+00:00</sy:updateBase>
-    <items/>
   </channel>
 </rdf:RDF>
 
-RSS;
+Feed;
 
-        $feed->formatOutput(true);
-        $feed->setTitle('foobar');
-        $this->assertSame($output, $feed->render());
+        $rdf->setTitle('foobar');
+        $this->assertSame($output, $document->saveXML());
     }
 
     /**
-     * @dataProvider feedProvider
+     * @dataProvider rdfProvider
      */
-    public function testSetUrl($feed)
+    public function testSetUrl($rdf, $document)
     {
-        $generator = Feed::URI;
-        $date      = DateTime::createFromFormat('Y-m-d', date('Y-m-d'))->format(DateTime::ATOM);
-        $output    = <<<RSS
-<?xml version="1.0" encoding="UTF-8"?>
-<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:admin="http://webns.net/mvcb/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns="http://purl.org/rss/1.0/">
-  <channel rdf:about="http://example.com/">
-    <admin:generatorAgent rdf:resource="$generator"/>
-    <dc:date>$date</dc:date>
-    <sy:updatePeriod>hourly</sy:updatePeriod>
-    <sy:updateFrequency>1</sy:updateFrequency>
-    <sy:updateBase>2000-01-01T12:00+00:00</sy:updateBase>
-    <items/>
-  </channel>
+        $output = <<<Feed
+<?xml version="1.0"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://purl.org/rss/1.0/">
+  <channel rdf:about="http://example.com/"/>
 </rdf:RDF>
 
-RSS;
+Feed;
 
-        $feed->formatOutput(true);
-        $feed->setUrl('foobar');
-        $this->assertSame($output, $feed->render());
+        $rdf->setUrl('foobar');
+        $this->assertSame($output, $document->saveXML());
     }
 }
